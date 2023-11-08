8a2b529fb3e18d8ebc980522fa1925d290ae5b27
Serm357 <136128813+Serm357@users.noreply.github.com>
2023-11-08 15:20:03 +0000

    solving cors problems in old browsers

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
+helmet({
+  crossOriginResourcePolicy: false,
+})
 let users = [
   {
     id: 1,
